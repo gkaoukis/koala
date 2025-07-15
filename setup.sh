ca176ee376bf5fa08f986ff3b01e3a07c421e42e
gkaoukis <geoka001@gmail.com>
2025-07-15 14:17:35 +0300

    fix in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ TOP=$(git rev-parse --show-toplevel)
 sudo apt-get update
 sudo apt-get install -y  git autoconf automake libtool build-essential cloc time gawk jq strace lsof python3 python3-pip python3-venv
 VENV_DIR="$TOP/venv"
-if [ ! -d "$VENV_DIR" ]; then
-    python3 -m venv "$VENV_DIR"
-fi
+rm -rf "$VENV_DIR"
+python3 -m venv "$VENV_DIR"
 source "$VENV_DIR/bin/activate"
+
 pip install --upgrade pip
 pip install --break-system-packages -r "$TOP/infrastructure/requirements.txt"
